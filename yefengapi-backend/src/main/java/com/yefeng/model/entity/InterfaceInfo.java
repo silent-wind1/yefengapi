0d230d5b4effbc4aea525bdde91c9bb0b9095ba3
yefeng <1797719651@qq.com>
2024-04-30 00:01:06 +0800

    feat(yefengapi-backend): init

---
+++ yefengapi-backend/src/main/java/com/yefeng/model/entity/InterfaceInfo.java
@@ -0,0 +1,44 @@
+package com.yefeng.model.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * @TableName interface_info
+ */
+@TableName(value ="interface_info")
+@Data
+public class InterfaceInfo implements Serializable {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String url;
+
+    private String requestHeader;
+
+    private String responseHeader;
+
+    private Integer status;
+
+    private String method;
+
+    private Long userId;
+
+    private Date createTime;
+
+    private Date updateTime;
+
+    @TableLogic
+    private Integer isDeleted;
+
+    private static final long serialVersionUID = 1L;
+
+}
